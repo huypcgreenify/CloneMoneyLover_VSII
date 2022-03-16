60722cc0908408bee02c4f349fddf72136d3109a
huypro123 <62203494+huypro123@users.noreply.github.com>
2022-03-16 16:22:08 +0700

    feat/register

--- src/features/index.js
+++ src/features/index.js
@@ -1,9 +1,4 @@
-// import Welcome from './Welcome/screens/Welcome' 
 import Register from './Register/screens/Register' 
-// import Login from './Login/screens/Login' 
-// import TransactionBook from './TransactionBook/screens/TransactionBook' 
-// import AddWallet from './AddWallet/screens/AddWallet'
-// // import Profile from './Proflie/screens/Profile'
 
 export {
     Register,
