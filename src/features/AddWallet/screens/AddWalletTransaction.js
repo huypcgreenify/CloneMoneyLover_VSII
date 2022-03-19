b3556be5a46d7043a7928f2df3c6207797bfd87a
huypro123 <62203494+huypro123@users.noreply.github.com>
2022-03-19 22:52:37 +0700

    fix

--- src/features/AddWallet/screens/AddWalletTransaction.js
+++ src/features/AddWallet/screens/AddWalletTransaction.js
@@ -1,11 +1,9 @@
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
 import {
     Text,
     View,
     Image,
     TouchableOpacity,
-    Dimensions,
-    useWindowDimensions,
     ScrollView,
     TextInput
 } from 'react-native'
